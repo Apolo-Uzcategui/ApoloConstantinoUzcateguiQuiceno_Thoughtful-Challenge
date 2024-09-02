9a39f3cd891c1e8c249c2698a72f873395d10d22
Apolo-Uzcategui <apolo.uzcategui@gmail.com>
2024-09-01 23:37:21 -0500

    Adding the text analyzer to filter if money is being mentioned, and also added the output excel file with the information from the class object

--- process_news.py
+++ process_news.py
@@ -1,21 +1,50 @@
+"""
+This code is an RPA automation made with the Robocorp framework to get information
+from web articles, in this case molded to work for https://www.latimes.com/
+We require the following data:
+• Title
+• Date
+• Author
+• Image file
+• Image description
+• Article URL
+
+With this data, we create an Excel file with this information.
+
+This is done with the use of up-to-date Python programming, OOP, no automation, 
+no API, and uploaded with GitHub integration to the Robocloud platform.
+
+Ideas to expand: A new workitem value can be added, which can control the 
+time frame of the articles retrieved. This would require a loop that keeps True
+as long as the date of the article (can be seen in the search before clicking
+on the article) meets the criteria of the timeframe
+
+Made by Apolo Uzcátegui, Colombian developer.
+"""
+
+
+"""
+Robocorp + RPA libraries used
+"""
 from robocorp.tasks import task
 from robocorp import workitems
 from robocorp import browser
 from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
+from RPA.Tables import Table
 from RPA.Browser.Selenium import Selenium
 
-class search_config:
-    def __init__(self, phrase, category, time):
-        self.phrase = phrase
-        self.category = category
-        self.time = time
+import re
 
-    @classmethod
-    def get_data(cls, search_parameters):
-        search_phrase = search_parameters[""]
-        category = work_item.get("category")
-        months_back = int(work_item.get("months_back", 0))
-        return cls(search_phrase, category, months_back)
+class Article:
+    def __init__(self, title, date, author, img, img_desc, money_mention, link):
+        self.title = title
+        self.date = date
+        self.author = author
+        self.img = img
+        self.img_desc = img_desc
+        self.money_mention = money_mention
+        self.link = link
     
 """
 Category options:
@@ -51,15 +80,20 @@ def process_news():
     )
 
     search_parameters = get_workitem()
-    message = search_parameters["time"]
 
     search_news(search_parameters)
     open_article()
-    title, date, author, link =  get_article_data()
-    print(title)
-    print(date)
-    print(author)
-    print(link)
+    article =  get_article_data()
+    """
+    print(article.title)
+    print(article.date)
+    print(article.author)
+    print(article.img)
+    print(article.img_desc)
+    print(article.money_mention)
+    print(article.link)
+    """
+    create_output_file(article)
 
 def get_workitem():
     """
@@ -78,29 +112,32 @@ def search_news(search_parameters):
     The clicks and fills to get to the articles are executed.
     The clicks necessary to configure the search are done.
     """
-
+    #Search related variables
     news_portal = "https://www.latimes.com/"
     search_tab = ".xs-5\:h-6"
     search_text = "[name=q]"
     search_button = ".h-6\.25"
     order_by = "select.select-input[name='s']"
+    article_type = ('input[name="f1"][value="8fd31d5a-5e1c-3306-9f27-6edc9b08423e"]')
 
-    #nth-child(1) li:nth-child(1) .checkbox-input-element:nth-child(1)
+    # Article related variables
     phrase = search_parameters["phrase"]
     category = categories[search_parameters["category"]]
     time = sorts[search_parameters["time"]]
-    article_type = ('input[name="f1"][value="8fd31d5a-5e1c-3306-9f27-6edc9b08423e"]')
-
+    
+    # Steps to open the page
     browser.goto(news_portal)
     page = browser.page()
     page.wait_for_timeout(2000) 
 
+    # Steps to make the phrase search
     page.click(search_tab)
     page.fill(search_text, phrase)
     page.click(search_button)
     page.wait_for_timeout(2000) 
     reload_page()
 
+    # Steps to change the filters
     page.click(category)
     page.click(article_type)
     page.select_option(order_by, time)
@@ -108,10 +145,16 @@ def search_news(search_parameters):
     reload_page()
 
 def reload_page():
+    """
+    Does a page reload, can be used to avoid modals or timeouts
+    """
     page = browser.page()
     browser.page().reload()
 
 def open_article():
+    """
+    Opens the first article from a search
+    """
     page = browser.page()
 
     first_article = "li:nth-child(1) h3.promo-title a"
@@ -120,15 +163,78 @@ def open_article():
     page.wait_for_timeout(500) 
 
 def get_article_data():
+    """
+    Gets the information needed from the article.
+    Returns an object of the article class.
+    """
     page = browser.page()
+
+    text_from_article = get_article_text()
+
     title = page.locator(".headline").inner_text()
     date = page.locator(".published-date-day").inner_text()
     author = page.locator(".authors .author-name .link").inner_text()
     img = page.locator(".page-lead-media .figure img[src]").get_attribute("src")
     img_desc =page.locator(".page-lead-media .figure .image").get_attribute("alt")
+    money_mention = find_money_mentions(text_from_article)
     link = page.url
-    print(img)
-    print(img_desc)
-    page.wait_for_timeout(10000) 
 
-    return title, date, author, link
+    article = Article(title, date, author, img, img_desc, money_mention, link)
+    
+
+    page.wait_for_timeout(5000) 
+
+    return article
+
+def get_article_text():
+    """
+    Analyzes the text div from the DOM by joining all "p" elements.
+    """
+    page = browser.page()
+
+    paragraphs = page.locator('div[data-element="story-body"] p')
+    paragraph_count = paragraphs.count()
+    article_text = []
+
+    for i in range(paragraph_count):
+        text = paragraphs.nth(i).text_content()
+        article_text.append(text)
+
+    full_article = "\n\n".join(article_text)
+
+    return full_article
+
+def find_money_mentions(text):
+    """
+    Define a regex pattern to match money formats required
+    """
+    pattern = r'(\$\d{1,3}(,\d{3})*(\.\d{2})?)|(\d+\s+(dollars|USD))'
+    matches = re.findall(pattern, text)
+    
+    money_mentions = [match[0] if match[0] else match[3] for match in matches]
+    
+    print(money_mentions)
+
+    if not money_mentions:
+        money_mention = False
+    else:
+        money_mention = True
+
+    return money_mention
+
+def create_output_file(article):
+    header = ["Title", "Date", "Author", "Image filename", "Image description", "Money mention", "Link"]
+    data = [article.title, article.date, article.author, article.img, article.img_desc, article.money_mention, article.link]
+
+    # Initialize the Excel library
+    excel = Files()
+
+    # Create a new Excel workbook
+    excel.create_workbook("output/output.xlsx")
+
+    # Append the header and data rows
+    excel.append_rows_to_worksheet([header, data], header=True)
+
+    # Save and close the workbook
+    excel.save_workbook()
+    excel.close_workbook()
